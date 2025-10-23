fc645acbe1fa96d61f0706a1758f452513a841bd
Taemely12345 <146439703+Taemely12345@users.noreply.github.com>
2025-10-23 10:52:37 -0300

    ,

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const caixaPrincipal - document.querySelector(".caixa-principal");
-const caixaPerguntas -document.querySelector("caixa-perguntas");
-const caixaAlternativas -document.querySelector("caixa-alternativas");
-const caixaResultado -document.querySelector("caixa-resultado");
-const textoResultado -document.querySelector("texto-resultado");
+const caixaPrincipal =document.querySelector(".caixa-principal");
+const caixaPerguntas =document.querySelector("caixa-perguntas");
+const caixaAlternativas =document.querySelector("caixa-alternativas");
+const caixaResultado =document.querySelector("caixa-resultado");
+const textoResultado =document.querySelector("texto-resultado");
 
-const perguntas -[
+const perguntas =[
     {
         enunciando:
     Qual o futuro da IA?
@@ -92,3 +92,8 @@ function mostraPerguntas() {
     }
 
     function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada.afirmacao;
+        historiaFinal+= afirmacoes+" ";
+        atual++;
+        monstraPergunta();
+    }
